4af249472dda79a5a9dbbfc23764c3d5d9311b8f
bbartmanBI <Brian.bartman@beyondidentity.com>
2024-08-22 08:16:52 -0400

    Ran formatter.

--- src/schema/ws_fed/mod.rs
+++ src/schema/ws_fed/mod.rs
@@ -1,26 +1,26 @@
+pub mod address;
 pub mod applies_to;
 pub mod constants;
 pub mod endpoint_reference;
+pub mod key_type;
+pub mod life_time;
 pub mod request_security_token_response;
+pub mod request_type;
+pub mod requested_reference;
 pub mod requested_security_token;
 pub mod token_type;
-pub mod address;
-pub mod key_type;
-pub mod request_type;
-pub mod life_time;
-pub mod requested_reference;
 
+pub use address::*;
 pub use applies_to::*;
 pub use constants::*;
 pub use endpoint_reference::*;
+pub use key_type::*;
+pub use life_time::*;
 pub use request_security_token_response::*;
+pub use request_type::*;
+pub use requested_reference::*;
 pub use requested_security_token::*;
 pub use token_type::*;
-pub use address::*;
-pub use key_type::*;
-pub use request_type::*;
-pub use life_time::*;
-pub use requested_reference::*;
 
 use super::*;
 use serde::Deserialize;
