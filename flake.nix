b28166db62f38498eeb0214a9b10ab629fae7bb5
Josh Robson Chase <josh@robsonchase.com>
2025-03-24 14:57:41 -0400

    bump nixpkgs version, improve approach to build inputs and bindgen

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
     rust-overlay = {
@@ -32,7 +32,7 @@
               nix-filter = nix-filter.lib;
               rust-toolchain = pkgs.rust-bin.stable.latest.default;
               rust-dev-toolchain = pkgs.rust-toolchain.override {
-                extensions = [ "rust-src" ];
+                extensions = [ "rust-src" "rust-analyzer" ];
               };
             })
           ];
@@ -42,16 +42,18 @@
           craneLib =
             (crane.mkLib pkgs).overrideToolchain pkgs.rust-toolchain;
           lib = pkgs.lib;
-          stdenv = pkgs.stdenv;
           commonNativeBuildInputs = with pkgs; [
+            rustPlatform.bindgenHook
+            pkg-config
+          ];
+          commonBuildInputs = with pkgs;[
             libiconv
             libtool
             libxml2
             libxslt
-            llvmPackages.libclang
-            openssl
-            pkg-config
             xmlsec
+            openssl
+            llvmPackages.libclang
           ];
           fixtureFilter = path: _type:
             builtins.match ".*test_vectors.*" path != null ||
@@ -65,24 +67,8 @@
           commonArgs = {
             inherit src;
 
-            # Need to tell bindgen where to find libclang
-            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-
-            # Set C flags for Rust's bindgen program. Unlike ordinary C
-            # compilation, bindgen does not invoke $CC directly. Instead it
-            # uses LLVM's libclang. To make sure all necessary flags are
-            # included we need to look in a few places.
-            # See https://web.archive.org/web/20220523141208/https://hoverbear.org/blog/rust-bindgen-in-nix/
-            BINDGEN_EXTRA_CLANG_ARGS = "${builtins.readFile "${stdenv.cc}/nix-support/libc-crt1-cflags"} \
-                ${builtins.readFile "${stdenv.cc}/nix-support/libc-cflags"} \
-                ${builtins.readFile "${stdenv.cc}/nix-support/cc-cflags"} \
-                ${builtins.readFile "${stdenv.cc}/nix-support/libcxx-cxxflags"} \
-                -idirafter ${pkgs.libiconv}/include \
-                ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
-                ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
-            ";
-
             nativeBuildInputs = commonNativeBuildInputs;
+            buildInputs = commonBuildInputs;
           };
           # Build *just* the cargo dependencies, so we can reuse
           # all of that work (e.g. via cachix) when running in CI
@@ -91,30 +77,13 @@
             inherit cargoArtifacts;
           });
         in
-        rec {
+        {
           # `nix build`
           packages.default = samael;
 
           # `nix develop`
           devShells.default = pkgs.mkShell {
-            # Need to tell bindgen where to find libclang
-            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-
-            # Set C flags for Rust's bindgen program. Unlike ordinary C
-            # compilation, bindgen does not invoke $CC directly. Instead it
-            # uses LLVM's libclang. To make sure all necessary flags are
-            # included we need to look in a few places.
-            # See https://web.archive.org/web/20220523141208/https://hoverbear.org/blog/rust-bindgen-in-nix/
-            BINDGEN_EXTRA_CLANG_ARGS = "${builtins.readFile "${stdenv.cc}/nix-support/libc-crt1-cflags"} \
-                ${builtins.readFile "${stdenv.cc}/nix-support/libc-cflags"} \
-                ${builtins.readFile "${stdenv.cc}/nix-support/cc-cflags"} \
-                ${builtins.readFile "${stdenv.cc}/nix-support/libcxx-cxxflags"} \
-                -idirafter ${pkgs.libiconv}/include \
-                ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
-                ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
-            ";
-
-            buildInputs = with pkgs; [ rust-dev-toolchain nixpkgs-fmt ];
+            buildInputs = with pkgs; [ rust-dev-toolchain nixpkgs-fmt ] ++ commonBuildInputs;
             nativeBuildInputs = commonNativeBuildInputs;
             shellHook = ''
               export DIRENV_LOG_FORMAT=""
